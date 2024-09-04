7bf49aa1f12e78592599831d3943ee1d92b9a149
adamsafril19 <adamsafrila0@gmail.com>
2024-09-04 10:14:54 +0700

    Praktikum 1 - JS 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,42 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('/hello', function(){
+    return 'Hello';
+});
+
+Route::get('/world', function(){
+    return 'world';
+});
+
+Route::get('/', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return '2241760058 Adam Safrila I';
+});
+
+Route::get('/user/{adam}', function($name){
+    return 'Nama Saya '. $name;
+});
+
+Route::get('/posts/{post}/comments/{comments}', function($postId, $commentsId){
+    return 'Post ke-'. $postId.' Komentar ke-'. $commentsId;
+});
+
+Route::get('/articel/{id}', function($articelId){
+    return 'Halaman Artikel dengan ID '. $articelId;
+});
+
+Route::get('/user/{adam?}', function($name=null){
+    return 'Nama Saya '. $name;
+});
+
+Route::get('/user/{adam?}', function($name='john'){
+    return 'Nama Saya '. $name;
+});
+
 
 /*
 |--------------------------------------------------------------------------
