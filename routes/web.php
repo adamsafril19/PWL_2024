53beb6862e9b57419c911456f611ebfa60e22892
adamsafril19 <adamsafrila0@gmail.com>
2024-09-06 21:03:47 +0700

    Soal Praktikum POS - JS 2

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\TransaksiController;
+
 
 // Route::get('/hello', function(){
 //     return 'Hello';
@@ -23,9 +25,9 @@ Route::get('/about', function(){
     return '2241760058 Adam Safrila I';
 });
 
-Route::get('/user/{adam}', function($name){
-    return 'Nama Saya '. $name;
-});
+// Route::get('/user/{adam}', function($name){
+//     return 'Nama Saya '. $name;
+// });
 
 Route::get('/posts/{post}/comments/{comments}', function($postId, $commentsId){
     return 'Post ke-'. $postId.' Komentar ke-'. $commentsId;
@@ -35,13 +37,13 @@ Route::get('/articel/{id}', function($articelId){
     return 'Halaman Artikel dengan ID '. $articelId;
 });
 
-Route::get('/user/{adam?}', function($name=null){
-    return 'Nama Saya '. $name;
-});
+// Route::get('/user/{adam?}', function($name=null){
+//     return 'Nama Saya '. $name;
+// });
 
-Route::get('/user/{adam?}', function($name='john'){
-    return 'Nama Saya '. $name;
-});
+// Route::get('/user/{adam?}', function($name='john'){
+//     return 'Nama Saya '. $name;
+// });
 
 
 /*
@@ -73,3 +75,35 @@ Route::get('/greeting', function() {
 });
 
 Route::get('/greeting', [WelcomeController::class,'greeting']);
+
+//  POS
+
+// home
+Route::get('/home', function(){
+    return view('home');
+});
+
+// user
+Route::get('/user/{id}/name/{name}', function($id='null',$name='null'){
+    return ''.$id.'  '.$name;
+});
+
+//products
+Route::prefix('/products')->group(function () {
+    Route::get('/category/food-beverage', function(){
+        return 'Halaman food-beverage';
+    });
+    Route::get('/category/beauty-health', function(){
+        return 'Halaman beauty-health';
+    });
+    Route::get('/category/home-care', function(){
+        return 'Halaman home-care';
+    });
+    Route::get('/category/baby-kid', function(){
+        return 'Halaman baby-kid';
+    });
+});
+
+//Penjualan
+Route::get('/transaksi', [TransaksiController::class,'transaksi']);
+
